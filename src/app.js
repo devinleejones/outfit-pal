1ea99b0cb211c055531ec5881d93f013e7167887
Devin Lee-Jones <devinleejones@gmail.com>
2018-10-23 15:14:24 -0700

    Used OpenWeatherMap API to fetch weather forecast data and log temperature data to the console. #1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
+import Closet from './closet'
 
 const styles = {
   header: {
@@ -19,7 +20,19 @@ export default class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch(
+      'http://api.openweathermap.org/data/2.5/forecast?zip=92618&units=imperial&appid=d073dfb1703b47ce33c493182f9cacb9'
+    )
+      .then(result => result.json())
+      .then(data => {
+        this.setState({ weather: data })
+        console.log(this.state.weather.list[0].main)
+      })
+  }
+
   render() {
+    const { weather } = this.state
     return (
       <div className="container-fluid">
         <h1 style={styles.title}>OutfitPal</h1>
@@ -28,7 +41,7 @@ export default class App extends Component {
             {"This Week's Closet"}
           </Typography>
         </Grid>
-        {this.state.weather}
+        <Closet weather={weather} />
       </div>
     )
   }
