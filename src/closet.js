118986ef42a07ca523c875591c274e3005288bfc
Devin Lee-Jones <devinleejones@gmail.com>
2018-10-25 14:28:56 -0700

    Updated Closet component to render 5 unique weather card days. #1

--- src/closet.js
+++ src/closet.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react'
+import WeatherCard from './weatherCard'
+
+const styles = {
+  container: {
+    width: 1000,
+    // minWidth: '100%',
+    // overflowX: 'auto',
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  card: {}
+}
 
 export default class Closet extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      days: []
-    }
-  }
-
   render() {
     const { days } = this.props
+    console.log(days)
     return (
-      <div className="container-fluid">
-        <ul>
-          {days.map((day, index) => {
-            return (
-              <li key={index}>
-                <span>{day.condition}</span>
-                <span>{day.temperature}</span>
-                <span>{day.date}</span>
-              </li>
-            )
-          })}
-        </ul>
+      <div className="container-fluid mt-4" style={styles.container}>
+        {days.map((day, index) => {
+          return <WeatherCard key={index} style={styles.card} day={day} />
+        })}
       </div>
     )
   }
